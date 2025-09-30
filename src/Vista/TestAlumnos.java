39d99b0b8c69b3b7176c639896899d456410c929
Candela Naranjo <candenaranjo05@gmail.com>
2025-09-30 19:32:51 -0300

    vista-testAlumno

--- src/Vista/TestAlumnos.java
+++ src/Vista/TestAlumnos.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto_g14;
+package Vista;
 
 /**
  *
  * @author Anitabonita
  */
-public class Proyecto_G14 {
+public class TestAlumnos {
 
     /**
      * @param args the command line arguments
