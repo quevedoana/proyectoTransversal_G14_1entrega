d00a66eac4b092fd46991d327b25e4adc229730a
totolayo <totorastone@gmail.com>
2025-10-02 21:16:26 -0300

    Cree la clase materia
    Complete todo lo de la clase materia

--- src/Modelo/Materia.java
+++ src/Modelo/Materia.java
@@ -1,7 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+
 package Modelo;
 
 /**
@@ -9,5 +7,54 @@ package Modelo;
  * @author maria
  */
 public class Materia {
-    //TOTOS
+    
+    private int idMateria;
+    private String Nombre;
+    private int Año;
+    private boolean Estado;
+
+    public Materia(int idMateria, String Nombre, int Año, boolean Estado) {
+        this.idMateria = idMateria;
+        this.Nombre = Nombre;
+        this.Año = Año;
+        this.Estado = Estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getAño() {
+        return Año;
+    }
+
+    public void setAño(int Año) {
+        this.Año = Año;
+    }
+
+    public boolean isEstado() {
+        return Estado;
+    }
+
+    public void setEstado(boolean Estado) {
+        this.Estado = Estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", Nombre=" + Nombre + ", Año=" + Año + ", Estado=" + Estado + '}';
+    }
+    
 }
