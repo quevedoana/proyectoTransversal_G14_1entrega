23076a1acab5a1487614cd3fefd365d6b8940c81
AntonioAssat <turcotomas280302@gmail.com>
2025-10-06 21:20:10 -0300

    Clase inscripcion hecha

--- src/Modelo/Inscripcion.java
+++ src/Modelo/Inscripcion.java
@@ -10,4 +10,50 @@ package Modelo;
  */
 public class Inscripcion {
     //TURCO
+    private int idInscripto;
+    private int nota;
+    private int idAlumno;
+    private int idMateria;
+
+    public Inscripcion(int nota) {
+        this.nota = nota;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" + "idInscripto=" + idInscripto + ", nota=" + nota + ", idAlumno=" + idAlumno + ", idMateria=" + idMateria + '}';
+    }
+    
 }
